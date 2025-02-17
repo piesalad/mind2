63c0c50cb72d1be68c3a4ee45f38e38e52618c58
piesalad <169881939+piesalad@users.noreply.github.com>
2025-02-17 23:53:02 +0100

    Init repo

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import nextra from 'nextra'
+const withNextra = nextra({
+    latex: true
+    //mermaid: true,
+    // ... Other Nextra config options
+})
+
+// You can include other Next.js configuration options here, in addition to Nextra settings:
+export default withNextra({
+
+})
