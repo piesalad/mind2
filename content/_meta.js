02673332a12bf6b6170bd5125d50a75ae2ac6844
tfinn345 <169881939+piesalad@users.noreply.github.com>
2025-02-19 18:40:03 +0100

    various fixes and things

--- content/_meta.js
+++ content/_meta.js
@@ -2,7 +2,18 @@ export default {
     index: 'Introduction',
     gettingStarted: 'Getting started',
     verifiedPowerSupplies: 'Verified Power Supplies',
+    '###': {
+        type: 'separator'
+    },
+    developer: 'Developer',
     troubleshooting: {
         title: 'Troubleshooting'
+    },
+    '####': {
+        type: 'separator'
+    },
+    github_link: {
+        title: 'Jolla Mind 2 Official Documentation',
+        href: 'https://github.com/venhoai/jolla-mind2'
     }
 }
