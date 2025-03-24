5c2ac93925305cce05f41f8252b01379753393b5
tfinn345 <169881939+piesalad@users.noreply.github.com>
2025-03-24 23:05:13 +0100

    moved some guides from guides to /developer/guides

---
+++ content/developer/_meta.js
@@ -0,0 +1,10 @@
+export default {
+    debuggingMind2: 'Debugging the Mind 2',
+    enableDevMode: 'Enable Dev Mode',
+    guides: {
+        title: 'Guides',
+        theme: {
+            "collapsed": true
+        }
+    }
+}
