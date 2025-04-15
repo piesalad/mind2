768c573e32c5c28e77104797a2ed445091d5e082
tfinn345 <169881939+piesalad@users.noreply.github.com>
2025-04-15 09:44:28 +0200

    added reset instructions

--- content/developer/_meta.js
+++ content/developer/_meta.js
@@ -1,5 +1,5 @@
 export default {
-    debuggingMind2: 'Debugging the Mind 2',
+    debuggingMind2: 'UART on the Mind 2',
     enableDevMode: 'Enable Dev Mode',
     guides: {
         title: 'Experimental guides',
